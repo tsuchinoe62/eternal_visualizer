e6f18bfc54dd7cc7aa136e3da585a9d30337125c
Tsuchinoe <fvfqcan@gmail.com>
2022-12-17 22:48:17 +0900

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,28 @@
 import React from 'react';
 import './index.css';
 import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
+  HomeOutlined,
   TeamOutlined,
-  UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Layout, Menu, theme } from 'antd';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps['items'] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
+  {key: "/", icon: React.createElement(HomeOutlined), label: "HOME"},
+  {key: "/players", icon: React.createElement(UserOutlined), label: "プレイヤー検索"},
+  {key: "/guilds", icon: React.createElement(TeamOutlined), label: "軍団検索"},
+]
 
 const App: React.FC = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const navigate = useNavigate();
 
   return (
     <Layout hasSider>
@@ -48,23 +37,38 @@ const App: React.FC = () => {
         }}
       >
         <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <Menu
+          onClick={({key})=>{
+            navigate(key);
+          }}
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['/']}
+          items={items}
+        />
       </Sider>
       <Layout className="site-layout" style={{ marginLeft: 200 }}>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        {/* <Header style={{ padding: 0, background: colorBgContainer }}></Header> */}
         <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
-            <p>long content</p>
-            {
-              // indicates very long content
-              Array.from({ length: 100 }, (_, index) => (
-                <React.Fragment key={index}>
-                  {index % 20 === 0 && index ? 'more' : '...'}
-                  <br />
-                </React.Fragment>
-              ))
-            }
-          </div>
+        <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
+          <p>long content</p>
+          {
+            // indicates very long content
+            Array.from({ length: 100 }, (_, index) => (
+              <React.Fragment key={index}>
+                {index % 20 === 0 && index ? 'more' : '...'}
+                <br />
+              </React.Fragment>
+            ))
+          }
+        </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<div>Home</div>}></Route>
+            <Route path="/players" element={<div>Player</div>}></Route>
+            <Route path="/guilds"  element={<div>Guild</div>}></Route>
+          </Routes>
+        </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('container') as HTMLElement
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
